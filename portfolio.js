0718df47b608dfa31d3021cdda591d32856acab8
Francesiyamu <francessiyamu01@gmail.com>
2024-06-01 17:51:48 +0200

    Resume, Contact, Active link.
    Resume and contact Section added in html, styled with css and made responsive with media queries.
    Active link state on scroll added with JS.

--- portfolio.js
+++ portfolio.js
@@ -17,3 +17,27 @@ let typed = new Typed('.auto-input',{
     backDelay: 2000,
     loop: true,
 })
+
+
+//Active link State on Scroll
+
+//Get all links
+
+let navLinks = document.querySelectorAll('nav ul li a');
+ //Get all Sections
+ let sections = document.querySelectorAll('section')
+
+
+ window.addEventListener('scroll', function(){
+  const scrollPos = window.scrollY + 20
+  sections.forEach(section => {
+    if(scrollPos > section.offsetTop && scrollPos < (section.offsetTop + section.offsetHeight)){
+      navLinks.forEach(link => {
+        link.classList.remove('active'); 
+        if(section.getAttribute('id') === link.getAttribute('href').substring(1)) {
+          link.classList.add('active')
+        }
+      });
+    }
+  });
+ });
