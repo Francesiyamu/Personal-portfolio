bb5fa2adb483617b7267d7e94c0624393b951a48
Francesiyamu <francessiyamu01@gmail.com>
2024-06-02 17:58:42 +0200

    form validation
    Contact form validation and encryption

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # Personal-portfolio
+
+
+
+Elastic Email:
+https://elasticemail.com/
+
+
+# SweetAlert2
+pop up box after form submission: https://sweetalert2.github.io/
+icons: 
+
+
+
+# Smptjs
+https://smtpjs.com/
+https://elasticemail.com/
+At the contact validation, smptjs was used to send emails directly from JavaScript without the need for a backend server.
+I started by including SmtpJs in my project via a CDN. An event listener was added on the form which listend for the 'submit' event, prevents the default submission using e.prevetntDefault. In de 'sendEmail' Function, the email body is constructed using the form values and sends the email SmtpJs. A secure token was generated to encrypt the credentials. 
--- portfolio.js
+++ portfolio.js
@@ -19,7 +19,7 @@ let typed = new Typed('.auto-input',{
 })
 
 
-//Active link State on Scroll
+/*------Active link State on Scroll------*/
 
 //Get all links
 
@@ -41,3 +41,67 @@ let navLinks = document.querySelectorAll('nav ul li a');
     }
   });
  });
+
+
+
+
+ /*-----contact form validation-----*/
+
+const form = document.querySelector("form");
+const fullName = document.getElementById("fname");
+const email = document.getElementById("email");
+const subject = document.getElementById("subject");
+const message = document.getElementById("msg");
+
+
+//construct the email body using the form values
+ function sendEmail() {
+  const bodyMessage = `
+  Full name: ${fname.value}<br>
+  Email: ${email.value}<br>
+  Subject: ${subject.value}<br>
+  Message: ${msg.value}
+`;
+
+//send the email
+      Email.send({
+        SecureToken : "2a2197bb-aeec-4a4b-9bd5-9f76c7f2717c", //Encrypts SMTP credentials.
+        Username : "francessiyamu01@gmail.com",
+        Password : "A6515F7D46E31C82734DEE5594860589D6BD",
+        To : 'francessiyamu01@gmail.com',
+        From : "francessiyamu01@gmail.com",
+        Subject : subject.value,
+        Body : bodyMessage,
+    }).then(
+      //pop up message
+      message => {
+        if (message == "OK"){
+          Swal.fire({
+            title: "Success!",
+            text: "Message sent successfully!",
+            icon: "success"
+          });
+        }
+      }
+      
+    );
+ }
+
+ 
+
+
+
+ form.addEventListener("submit", (e) => {
+  e.preventDefault(); //prevents the default form submission
+ 
+  sendEmail();
+
+
+  //reset the input field
+  form.reset();
+  return false;
+
+ });
+
+
+
