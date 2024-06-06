8fac6f138c84d4a2feb2ec858e8101be28cc4f77
Francesiyamu <francessiyamu01@gmail.com>
2024-06-06 17:04:34 +0200

    Serve the html page using node js and express.

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const app = express() //start an express app;
+const path = require('path');
+
+
+//Define routes
+
+/*app.get('/', (req, res) => {
+
+
+  //Send a response
+  res.json("It works!");
+});*/
+
+const router = express.Router();
+
+
+app.use(express.static('public'));
+
+
+
+router.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/portfolio.html"));
+})
+
+
+
+app.use("/", router);
+
+
+app.listen(8080, () =>{
+  console.log("App is starting at port", 8080);
+})
